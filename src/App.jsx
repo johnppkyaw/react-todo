4b7fe97e57cc3f7ad42007260ff1fce4e5c7109e
John Kyaw <jjpyae@gmail.com>
2024-11-03 15:53:50 -0500

    Assignment complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const todoList = [
+    {
+      id: 1,
+      title: "Create the assignment"
+    },
+    {
+      id: 2,
+      title: "Follow the instructions"
+    },
+    {
+      id: 3,
+      title: "Complete the assignment"
+    }
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo List</h1>
+      <ul>
+        {
+          todoList.map(function(eachTask) {
+            return (
+              <li key={eachTask.id}>{eachTask.title}</li>
+            )
+          })
+        }
+      </ul>
     </>
   )
 }
