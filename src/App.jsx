cfb4c0682cf1728369620d1334d19e6cd79663c2
John Kyaw <jjpyae@gmail.com>
2024-11-10 16:24:26 -0500

    Create AddTodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import TodoList from './TodoList.jsx'
+import AddTodoForm from './AddTodoForm.jsx'
 import './App.css'
 
 function App() {
   return (
     <>
+      <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </>
   )
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -15,7 +15,6 @@ const TodoList = () => {
   ]
   return (
     <>
-      <h1>Todo List</h1>
       <ul>
         {
           todoList.map(function(eachTask) {
