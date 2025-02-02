d17200d440f6ac635ec0c7688b22e016e15cf517
John Kyaw <jjpyae@gmail.com>
2025-02-02 17:12:36 -0500

    Improve project structure and assign component prop types

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,10 +1,17 @@
 import styles from './TodoListItem.module.css';
+import PropTypes from 'prop-types';
 
 const TodoListItem = (prop) => {
   const { taskId, task, onRemoveTodo } = prop;
   return (
     <li className={styles.ListItem}>{task}<button onClick={()=>{onRemoveTodo(taskId)}}>Remove</button></li>
   )
-
 }
+
+TodoListItem.propTypes = {
+  taskId: PropTypes.string,
+  task: PropTypes.string,
+  onRemoveTodo: PropTypes.func
+}
+
 export default TodoListItem
